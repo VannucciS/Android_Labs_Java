11dedd54e6445bf6ec4b6b7e49b4f267572e82c7
Vannucci <homelete@gmail.com>
2024-01-27 10:10:22 -0500

    second commit - three types of buttons with the same behavior

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -5,10 +5,18 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.Switch;
 import android.widget.TextView;
 
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,13 +26,31 @@ public class MainActivity extends AppCompatActivity {
         TextView mytext = findViewById(R.id.textview);
         EditText myedit = findViewById(R.id.edittext);
         Button mybutton = findViewById(R.id.mybutton);
-        String edittext = myedit.getText().toString();
+
 
         mybutton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                mytext.setText("Your edit text has: $edittext");
+                String edittext = myedit.getText().toString();
+                mytext.setText("Your edit text has:" + edittext);
             }
+        });
+
+        CheckBox checkBox = findViewById(R.id.checkbox);
+        Switch myswitch = findViewById(R.id.myswitch);
+        RadioButton radio = findViewById(R.id.radio);
+
+        checkBox.setOnCheckedChangeListener((buttonView, isChecked) -> {
+            myswitch.setChecked(isChecked);
+            radio.setChecked(isChecked);
+        });
+        myswitch.setOnCheckedChangeListener((buttonView, isChecked) -> {
+            checkBox.setChecked(isChecked);
+            radio.setChecked(isChecked);
+        });
+        radio.setOnCheckedChangeListener((buttonView, isChecked) -> {
+            myswitch.setChecked(isChecked);
+            checkBox.setChecked(isChecked);
         });
 
     }
